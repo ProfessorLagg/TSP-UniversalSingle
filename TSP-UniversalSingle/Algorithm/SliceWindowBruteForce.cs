c308c94e9fd4f4bae56ff8df4a52898658dd083a
ProfessorLagg <93381546+ProfessorLagg@users.noreply.github.com>
2022-12-12 18:50:55 +0100

    Transfered PermutationGenerator, including perm files. And SliceWindowBruteForce from Dev project

---
+++ TSP-UniversalSingle/Algorithm/SliceWindowBruteForce.cs
@@ -0,0 +1,67 @@
+﻿using Combinatorics.Collections;
+using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TSPStandard.Algorithm
+{
+    public class SliceWindowBruteForce : TSPAlgorithm
+    {
+        public SliceWindowBruteForce(TSPRoute route) : base(route)
+        {
+            this.AlgorithmType = AlgorithmType.SliceWindowBruteForce;
+        }
+        private static PermutationGenerator permGen = new();
+        public int PermLength = 8;
+        public bool Recursive = false;
+        public override void Run()
+        {
+            List<int[]> IndexPerms = permGen.GetIndexPermutations(PermLength);
+        Start:
+            Span<Vector2> localSpan = new(this.Route.ToArray());
+            int L = localSpan.Length;
+
+            for (int i = 0; i < (L - PermLength); i++)
+            {
+                int preIndex = i - 1;
+                if (preIndex < 0) { preIndex += L; }
+                Vector2 preVector = localSpan[preIndex];
+                Vector2 postVector = localSpan[(i + 1 + PermLength) % L];
+
+                ReadOnlySpan<Vector2> originalSlice = new(localSpan.Slice(i, PermLength).ToArray());
+                float BestCost = originalSlice.TourLength() + preVector.Dist(originalSlice[0]) + originalSlice[originalSlice.Length - 1].Dist(postVector);
+                foreach (int[] indexPerm in CollectionsMarshal.AsSpan(IndexPerms))
+                {
+                    // calculating cost of slice if perm was applied
+                    float cost = postVector.Dist(originalSlice[indexPerm[indexPerm.Length - 1]]);
+                    Vector2 currVec = preVector;
+                    for (int j = 0; j < indexPerm.Length; j++)
+                    {
+                        cost += currVec.Dist(originalSlice[indexPerm[j]]);
+                        currVec = originalSlice[indexPerm[j]];
+                    }
+                    // Applying perm if cost of slice was better
+                    if (cost < BestCost)
+                    {
+                        var destSlice = localSpan.Slice(i, PermLength);
+                        for (int k = 0; k < destSlice.Length; k++)
+                        {
+                            destSlice[k] = originalSlice[indexPerm[k]];
+                        }
+                        BestCost = cost;
+                    }
+                }
+            }
+            TSPRoute bestFound = new(this.Route.RouteName, localSpan.ToArray());
+            if (bestFound.Cost < this.Route.Cost)
+            {
+                this.Route = new(bestFound);
+                if (Recursive) { goto Start; }
+            }
+        }
+    }
+}
---
+++ TSP-UniversalSingle/PermutationGenerator.cs
@@ -0,0 +1,178 @@
+﻿using System.Diagnostics;
+
+namespace TSPStandard
+{
+    public sealed class PermutationGenerator
+    {
+        private static string ExePath = Path.GetDirectoryName(System.Reflection.Assembly.GetEntryAssembly().Location);
+        private string PermFolderPath = Path.Combine(ExePath, "Assets", "permutations");
+        private Dictionary<int, string> PermutationSetFilePaths;
+        private static ConcurrentDictionary<int, List<int[]>> PermutationSetRamCache = new();
+        public static bool UseRamCache = false;
+        public bool BuildFileCache = false;
+        private void EnumeratePermutationSets()
+        {
+            PermutationSetFilePaths = new();
+            if (!Directory.Exists(PermFolderPath)) { Directory.CreateDirectory(PermFolderPath); }
+            List<string> filePaths = new(Directory.GetFiles(PermFolderPath));
+            var permFiles = filePaths.Where(p => Path.GetExtension(p) == ".perm");
+
+            foreach (string filePath in permFiles)
+            {
+                int length = Convert.ToInt32(Path.GetFileNameWithoutExtension(filePath));
+                PermutationSetFilePaths.TryAdd(length, filePath);
+            }
+            var csvFiles = filePaths.Where(p => Path.GetExtension(p) == ".csv");
+            foreach (string filePath in csvFiles)
+            {
+                int length = Convert.ToInt32(Path.GetFileNameWithoutExtension(filePath));
+                PermutationSetFilePaths.TryAdd(length, filePath);
+            }
+        }
+        public PermutationGenerator()
+        {
+            EnumeratePermutationSets();
+        }
+        public List<int[]> GetIndexPermutations(int permutationLength)
+        {
+            List<int[]> LoadOrCalcPerms()
+            {
+                if (permutationLength < 6 || !PermutationSetFilePaths.Keys.Contains(permutationLength))
+                {
+                    return GenerateIndexPermutations(permutationLength);
+                }
+                else
+                {
+                    return LoadIndexPermutations(permutationLength);
+                }
+            }
+
+            if (UseRamCache)
+            {
+                return PermutationSetRamCache.GetOrAdd(permutationLength, LoadOrCalcPerms());
+            }
+            else
+            {
+                return LoadOrCalcPerms();
+            }
+        }
+        private void SavePermutations(List<int[]> permutations)
+        {
+            // saving CSV
+            List<string> lines = new();
+            foreach (int[] perm in permutations)
+            {
+                string line = perm[0].ToString();
+                for (int i = 1; i < perm.Length; i++)
+                {
+                    line += ";" + perm[i].ToString();
+                }
+                lines.Add(line);
+            }
+            string fileNameCSV = permutations[0].Length.ToString() + ".csv";
+            string savePathCSV = Path.Combine(PermFolderPath, fileNameCSV);
+            File.WriteAllLines(savePathCSV, lines);
+            // saving .perm
+            string fileNamePerm = permutations[0].Length.ToString() + ".perm";
+            string savePathPerm = Path.Combine(PermFolderPath, fileNamePerm);
+            using (var fs = new FileStream(savePathPerm, FileMode.Create, FileAccess.Write))
+            {
+                foreach (int[] perm in permutations)
+                {
+                    for (int i = 0; i < perm.Length; i++)
+                    {
+                        byte[] bi = new byte[] { Convert.ToByte(perm[i]) };
+
+                        fs.Write(bi);
+                    }
+                }
+            }
+        }
+        private List<int[]> LoadIndexPermutations(int permutationLength)
+        {
+            List<int[]> result = new();
+            string loadPath = PermutationSetFilePaths[permutationLength];
+            string extention = Path.GetExtension(loadPath).ToLower();
+            switch (extention)
+            {
+                default:
+                    throw new ArgumentException("Cannot load file with type: " + extention);
+                case ".csv":
+                    LoadCSV();
+                    goto Exit;
+                case ".perm":
+                    LoadPerm();
+                    goto Exit;
+            }
+            void LoadCSV()
+            {
+                string[] lines = File.ReadAllLines(loadPath);
+                foreach (string line in new Span<string>(lines))
+                {
+                    string[] split = line.Split(';');
+                    int[] newPerm = new int[permutationLength];
+                    for (int i = 0; i < permutationLength; i++)
+                    {
+                        newPerm[i] = int.Parse(split[i]);
+                    }
+                    result.Add(newPerm);
+                }
+                if (BuildFileCache)
+                {
+                    string permPath = Path.GetFileNameWithoutExtension(loadPath) + ".perm";
+                    if (!File.Exists(permPath)) { SavePermutations(result); }
+                }
+            }
+            void LoadPerm()
+            {
+                byte[] bytes = File.ReadAllBytes(loadPath);
+                List<byte[]> chunks = bytes.Chunk(permutationLength).ToList(); // Splitting into permutation byte sized chunks
+                foreach (var chunk in chunks)
+                {
+                    List<int> newPerm = new();
+                    foreach (byte intByte in chunk)
+                    {
+                        newPerm.Add(Convert.ToInt32(intByte));
+                    }
+                    result.Add(newPerm.ToArray());
+                }
+                if (BuildFileCache)
+                {
+                    string csvPath = Path.GetFileNameWithoutExtension(loadPath) + ".csv";
+                    if (!File.Exists(csvPath)) { SavePermutations(result); }
+                }
+            }
+        Exit:
+            return result;
+        }
+        private List<int[]> GenerateIndexPermutations(int permutationLength)
+        {
+            List<int[]> result = new();
+            List<int> rootPerm = new(); for (int i = 0; i < permutationLength; i++) { rootPerm.Add(i); }
+            List<List<int>> tempResult = new();
+            for (int i = 0; i < permutationLength; i++) { tempResult.Add(new() { i }); }
+            while (tempResult[0].Count < permutationLength)
+            {
+                List<List<int>> oldResults = new List<List<int>>(tempResult);
+                tempResult.Clear();
+                foreach (List<int> oldResult in oldResults)
+                {
+                    var missing = rootPerm.Where(x => !oldResult.Contains(x));
+                    foreach (var miss in missing)
+                    {
+                        var newResult = new List<int>(oldResult);
+                        newResult.Add(miss);
+                        tempResult.Add(newResult);
+                    }
+                }
+            }
+            foreach (var temp in tempResult)
+            {
+                result.Add(temp.ToArray());
+            }
+            if (BuildFileCache) { SavePermutations(result); }
+
+            return result;
+        }
+    }
+}
